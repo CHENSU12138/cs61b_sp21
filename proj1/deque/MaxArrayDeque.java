38fab1d09a4fafd902edad05cf0ea33b71865355
cxycs61b <csy963327970@gmail.com>
2023-01-02 18:19:42 +0900

    proj1 done

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,33 @@
+package deque;
+import java.util.Comparator;
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    private Comparator<T> cmpT;
+    public MaxArrayDeque(Comparator<T> c){
+        super();
+        cmpT = c;
+    }
+    public T max(){
+        if (isEmpty()) {
+            return null;
+        }
+        T maxItem = this.get(0);
+        for (T i:this){
+            if (cmpT.compare(i, maxItem) > 0) {
+                maxItem = i;
+            }
+        }
+        return maxItem;
+    }
+    public T max(Comparator<T> c){
+        if (isEmpty()) {
+            return null;
+        }
+        T maxItem = this.get(0);
+        for (T i:this){
+            if (cmpT.compare(i, maxItem) > 0) {
+                maxItem = i;
+            }
+        }
+        return maxItem;
+    }
+}
